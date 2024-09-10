e01d1b13153e4ba90c7b82639916329f473b3f3c
EvgeniyHalimon <halimonevgeniy@gmail.com>
2024-09-10 16:07:07 +0300

    setup routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import { ArticleView, ArticlesView, AuthView, HomeView } from '@/views';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,16 +8,22 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: AuthView
+    },
+    {
+      path: '/articles',
+      name: 'articles',
+      component: ArticlesView
+    },
+    {
+      path: '/article/:id',
+      name: 'article',
+      component: ArticleView
     }
-
-    /* {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    } */
   ]
 });
 
