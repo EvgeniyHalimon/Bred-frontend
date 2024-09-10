f58478354884288e4d2e3a1efc8f1ce9afea2728
EvgeniyHalimon <halimonevgeniy@gmail.com>
2024-09-10 16:08:29 +0300

    fix error

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import { ArticleView, ArticlesView, AuthView, HomeView } from '@/views';
+import { ArticleView, ArticlesView, AuthView, HomeView } from '../views';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
