c82c039472016c59fe5b9f918a432810d4f9d631
EvgeniyHalimon <halimonevgeniy@gmail.com>
2024-09-13 09:57:07 +0300

    update logic

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
-import { getAccessToken } from './shared/tokenWorkshop';
-const isLoggedIn = ref(getAccessToken());
-console.log('🚀 ~ file: App.vue:6 ~ isLoggedIn:', isLoggedIn.value);
 </script>
 
 <template>
   <header>
     <div>
       <nav class="flex justify-between">
-        <div>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/articles">Articles</RouterLink>
-        </div>
-        <RouterLink to="/auth" v-if="!isLoggedIn">Auth</RouterLink>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/articles">Articles</RouterLink>
       </nav>
     </div>
   </header>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
 import { TheWelcome } from '../components';
+import { onMounted } from 'vue';
+import { getAccessToken } from '@/shared/tokenWorkshop';
+const router = useRouter();
+console.log('🚀 ~ file: HomeView.vue:5 ~ router:', router);
+
+onMounted(() => {
+  if (!getAccessToken()) {
+    router.push('/auth');
+  }
+});
 </script>
 
 <template>
