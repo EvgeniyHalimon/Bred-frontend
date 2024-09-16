3c47514f3c9f39047fae84470eee5ff31fa3f02c
EvgeniyHalimon <halimonevgeniy@gmail.com>
2024-09-16 18:57:57 +0300

    raw updates to home page

--- src/components/UserComponent.vue
+++ src/components/UserComponent.vue
@@ -14,11 +14,7 @@ console.log(props.user);
 
 <template>
   <div class="max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg">
-    <img
-      :src="`data:image/jpeg;base64,${user.photo}`"
-      :alt="`${user.firstName}-avatar`"
-      class="object-cover w-full h-56"
-    />
+    <img :src="user.photo" :alt="`${user.firstName}-avatar`" class="object-cover w-full h-56" />
     <div class="p-6">
       <h2 class="text-2xl font-semibold text-gray-800">{{ user.firstName }} {{ user.lastName }}</h2>
       <p class="text-gray-600">Bio: {{ user.bio }}</p>
