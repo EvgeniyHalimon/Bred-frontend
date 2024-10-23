036dda838da7c15af28d85fc1bce7273424e2a18
EvgeniyHalimon <halimonevgeniy@gmail.com>
2024-10-23 17:29:58 +0300

    update components

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,7 @@ import NavigationComponent from './components/NavigationComponent.vue';
 </script>
 
 <template>
-  <header class="sticky top-0 z-50 p-4 border-b-2 bg-neutral-900 border-lime-600">
-    <NavigationComponent />
-  </header>
+  <NavigationComponent />
 
   <main class="container p-6 mx-auto">
     <RouterView />
--- src/views/StarterView.vue
+++ src/views/StarterView.vue
@@ -1,25 +1,63 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue/dist/iconify.js';
+import { onMounted, onUnmounted, ref } from 'vue';
+
+const text =
+  ' - is a disorder of mental activity or something meaningless, incoherent, illogical. Make your own Bred.';
+const linkText = 'Get in!';
+const linkUrl = '/auth';
+const typingElement = ref<HTMLDivElement | null>(null);
+
+let index = 0;
+let timeoutId: number | null = null;
+
+const type = () => {
+  if (index < text.length && typingElement.value) {
+    if (typingElement.value.innerHTML.length === 0) {
+      typingElement.value.innerHTML = '<span class="italic">Bred</span>';
+    }
+    typingElement.value.innerHTML += text.charAt(index);
+
+    index++;
+    timeoutId = window.setTimeout(type, 100);
+  } else if (index === text.length) {
+    if (typingElement.value) {
+      typingElement.value.innerHTML += ` <a href="${linkUrl}" class="underline text-lime-600">${linkText}</a>`;
+      typingElement.value.classList.add('finished');
+    }
+  }
+};
+
+onMounted(type);
+
+onUnmounted(() => {
+  if (timeoutId !== null) {
+    clearTimeout(timeoutId);
+  }
+});
 </script>
 
 <template>
-  <div class="flex justify-center text-lime-600">
+  <div class="flex justify-center mb-6 text-lime-600">
     <div class="relative w-60 h-60">
-      <Icon icon="mdi:alpha-b-box-outline" class="absolute text-3xl fade-icon w-60 h-60" />
-      <Icon icon="mdi:alpha-b-box" class="absolute text-3xl fade-icon w-60 h-60" />
+      <Icon icon="mdi:alpha-b-box-outline" class="absolute fade-icon w-60 h-60" />
+      <Icon icon="mdi:alpha-b-box" class="absolute fade-icon w-60 h-60" />
     </div>
     <div class="relative w-60 h-60">
-      <Icon icon="mdi:alpha-r-box" class="absolute text-3xl fade-icon w-60 h-60" />
-      <Icon icon="mdi:alpha-r-box-outline" class="absolute text-3xl fade-icon w-60 h-60" />
+      <Icon icon="mdi:alpha-r-box" class="absolute fade-icon w-60 h-60" />
+      <Icon icon="mdi:alpha-r-box-outline" class="absolute fade-icon w-60 h-60" />
     </div>
     <div class="relative w-60 h-60">
-      <Icon icon="mdi:alpha-e-box-outline" class="absolute text-3xl fade-icon w-60 h-60" />
-      <Icon icon="mdi:alpha-e-box" class="absolute text-3xl fade-icon w-60 h-60" />
+      <Icon icon="mdi:alpha-e-box-outline" class="absolute fade-icon w-60 h-60" />
+      <Icon icon="mdi:alpha-e-box" class="absolute fade-icon w-60 h-60" />
     </div>
     <div class="relative w-60 h-60">
-      <Icon icon="mdi:alpha-d-box" class="absolute text-3xl fade-icon w-60 h-60" />
-      <Icon icon="mdi:alpha-d-box-outline" class="absolute text-3xl fade-icon w-60 h-60" />
+      <Icon icon="mdi:alpha-d-box" class="absolute fade-icon w-60 h-60" />
+      <Icon icon="mdi:alpha-d-box-outline" class="absolute fade-icon w-60 h-60" />
     </div>
+  </div>
+  <div class="flex justify-center">
+    <p ref="typingElement" class="typing"></p>
   </div>
 </template>
 
@@ -40,5 +78,39 @@ import { Icon } from '@iconify/vue/dist/iconify.js';
 
 .fade-icon:nth-child(2n) {
   animation-delay: 1s;
+}
+
+.typing {
+  font-family: monospace;
+  white-space: nowrap;
+  display: inline-block;
+  border-right: 8px solid;
+  animation:
+    typing 4s steps(30) forwards,
+    blink-caret 0.75s step-end infinite;
+  color: #65a30d;
+}
+
+@keyframes typing {
+  from {
+    width: 0;
+  }
+  to {
+    width: auto;
+  }
+}
+
+@keyframes blink-caret {
+  from,
+  to {
+    border-color: transparent;
+  }
+  50% {
+    border-color: #65a30d;
+  }
+}
+
+.typing.finished {
+  border-right: none;
 }
 </style>
