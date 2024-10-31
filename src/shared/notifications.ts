032bc0f3341e12f1aa31fac320003968ea1ffe14
EvgeniyHalimon <halimonevgeniy@gmail.com>
2024-10-31 10:26:44 +0200

    updated styles for notifications

--- src/shared/notifications.ts
+++ src/shared/notifications.ts
@@ -1,19 +1,20 @@
-import { Notify } from 'notiflix';
+import { Notify, type INotifyOptions } from 'notiflix';
 
 const { success, failure } = Notify;
 
+const options: INotifyOptions = {
+  timeout: 300000,
+  pauseOnHover: true,
+  position: 'right-bottom',
+  borderRadius: '0px',
+  closeButton: true,
+  clickToClose: true
+};
+
 export const showSuccessNotification = (message: string) => {
-  success(message, {
-    timeout: 3000,
-    pauseOnHover: true,
-    position: 'right-bottom'
-  });
+  success(message, { ...options, className: 'notification-clip-path success-notification' });
 };
 
 export const showErrorNotification = (message: string) => {
-  failure(message, {
-    timeout: 3000,
-    pauseOnHover: true,
-    position: 'right-bottom'
-  });
+  failure(message, { ...options, className: 'notification-clip-path failure-notification' });
 };
